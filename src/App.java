eda38fd77141fa426e100b322c7d53276005c5f3
Richardstroms <richard.stromstedt@elev.ga.ntig.se>
2023-09-13 10:27:11 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Divisionens Program:");
+        int x= 10;
+        int y= 10;
+        double d = (double)x/y;
+        System.out.println(x+"/"+y+" är kvoten = "+d);
+
+        double d_dec= 1.0;
+        int ett =(int) d_dec;
+        System.out.println(d_dec+" som heltal är "+ett);
+
+        System.out.println("");
+        System.out.println("");
+
+        System.out.println("Ord och ändelse program:");
+        String Ord1 ="Elefant";
+        String Ändelse1 ="er";
+        String Hela_ordet =Ord1+Ändelse1;
+
+        System.out.println("En " +Ord1+", Eller flera " +Hela_ordet);
+        
+        System.out.println("");
+
+
+
+    }
+}
